ee36e4a77ea098e2e53974f4c915c34dd5bb7b1f
mijeongseo <mimiseo@naraspace.com>
2021-08-26 15:19:17 +0900

    update

--- back-test/routes/posts.js
+++ back-test/routes/posts.js
@@ -1,27 +1,27 @@
-const express = require("express");
-const { isLoggedIn } = require("./middlewares");
+const express = require('express');
+const { isLoggedIn } = require('./middlewares');
 
-const { Image, User, Post } = require("../models");
+const { Image, Post } = require('../models');
 const router = express.Router();
 
-router.get("/", isLoggedIn, async (req, res, next) => {
-  //get /posts
+router.get('/', isLoggedIn, async (req, res, next) => {
+    //get /posts
 
-  try {
-    if (req.user) {
-      const posts = await Image.findAll({
-        where: {},
-        order: [["createdAt", "DESC"]],
-        include: [{ model: Post, where: { UserId: req.user.id }, attributes: ["UserId"] }],
-      });
-      res.status(201).json(posts);
-    } else {
-      // res.status(200).json(null);
+    try {
+        if (req.user) {
+            const posts = await Image.findAll({
+                where: {},
+                order: [['createdAt', 'DESC']],
+                include: [{ model: Post, where: { UserId: req.user.id }, attributes: ['UserId'] }],
+            });
+            res.status(201).json(posts);
+        } else {
+            // res.status(200).json(null);
+        }
+    } catch (err) {
+        console.error(err);
+        next(err);
     }
-  } catch (err) {
-    console.error(err);
-    next(err);
-  }
 });
 
 module.exports = router;
